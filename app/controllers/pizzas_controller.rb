85d3c2a90aa965a9569ca2799e20d8dc2ba8832e
iamkinglana <ngetichenock32@gmail.com>
2023-06-26 09:38:45 +0300

    Sec Commit

--- app/controllers/pizzas_controller.rb
+++ app/controllers/pizzas_controller.rb
@@ -1,2 +1,24 @@
 class PizzasController < ApplicationController
+
+  class DirectorsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+    def index
+      directors = Pizza.all
+      render json: directors
+    end
+
+    def show
+      director = Pizza.find(params[:id])
+      render json: director
+    end
+
+    private
+
+    def render_not_found_response
+      render json: { error: "Director not found" }, status: :not_found
+    end
+
+  end
+
 end
